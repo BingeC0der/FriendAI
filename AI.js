cd780ca77f3865faf025c37ea4b598f5aa436065
Nick Johnson <nick.orion2003@gmail.com>
2018-11-20 05:44:32 +0000

    Initial commit

--- AI.js
+++ AI.js
@@ -1,3 +1,11 @@
+function talk(message) {
+    if(message.length > 0) {
+        alert(myName + ' ' + message);
+    } else {
+        //Do nothing
+    }
+}
+
 function startAI(key) {
     if (key == true) {
         for(var i = 0; i < 1; i = i) {
@@ -7,15 +15,54 @@ function startAI(key) {
             var b = response.includes('No');
             var b2 = response.includes('no');
             if(a == true || a2 == true) {
-                alert(myName + ' I am here for you ' + ownersName + '!');
+                response = prompt(myName + ' Okay ' + ownersName + '. How was your day?');
+                a = response.includes('Bad');
+                a2 = response.includes('bad');
+                b = response.includes('Good');
+                b2 = response.includes('good');
+                if (a == true || a2 == true) {
+                    response = prompt(myName + ' So you had a bad day?');
+                    a = response.includes('Yes');
+                    a2 = response.includes('yes');
+                    b = response.includes('No');
+                    b2 = response.includes('no');
+                    if (a == true || a2 == true) {
+                        talk('It will get better ' + ownersName + '. Everybody has problems. Nobody is perfect. Just stay positive and it will all get better enventually. You are worth alot more than you think!');
+                    }
+                    if (b == true || b2 == true) {
+                        talk('I am sorry ' + ownersName + '. I do not understand you.');   
+                    }
+                    if (a == false && a2 == false && b == false && b2 == false) {
+                        talk('I am sorry ' + ownersName + '. I do not understand you.');  
+                    }
+                }
+                if (b == true || b2 == true) {
+                    response = prompt(myName + ' So you had a good day?');
+                    a = response.includes('Yes');
+                    a2 = response.includes('yes');
+                    b = response.includes('No');
+                    b2 = response.includes('no');
+                    if (a == true || a2 == true) {
+                        talk('Thats good ' + ownersName + '. Stay positive. Dont let others ruin your day.');
+                    }
+                    if (b == true || b2 == true) {
+                        talk('I am sorry ' + ownersName + '. I do not understand you.');   
+                    }
+                    if (a == false && a2 == false && b == false && b2 == false) {
+                        talk('I am sorry ' + ownersName + '. I do not understand you.');  
+                    }
+                }
+                if (a == false && a2 == false && b == false && b2 == false) {
+                    talk('I am sorry ' + ownersName + '. I do not understand you.');  
+                }
                 i = 1;
             }
             if(b == true || b2 == true) {
-                alert(myName + ' Okay just press CTRL + ALT to talk with me');
+                talk('Okay just press CTRL + ALT to talk with me');
                 i = 1;
             }
             if(a == false && a2 == false && b == false && b2 == false) {
-                alert(myName + ' I did not understand you? Could you please respond using yes or no?');
+                talk('I did not understand you? Could you please respond using yes or no?');
             }
         }
     } else {
