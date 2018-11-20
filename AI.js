7af33af4705a89977553598df5dcb6fe1c64ee25
Nick Johnson <nick.orion2003@gmail.com>
2018-11-20 04:38:37 +0000

    Initial commit

--- AI.js
+++ AI.js
@@ -1,7 +1,17 @@
-function startAI(key) {
-    if(key == true) {
-        alert(myName + ' Hello ' + ownersName + '!');
-    } else {
-        alert('Failed to initiate');
+alert(myName + ' Hello ' + ownersName + '!');
+for(var i = 0; i < 1; i = i) {
+    var response = prompt(myName + ' Do you need to talk to me?');
+    var a = response.includes('Yes' || 'yes');
+    var b = response.includes('No' || 'no');
+    if(a == true) {
+        alert(myName + ' I am here for you ' + ownersName + '!');
+        i = 1;
+    }
+    if(b == true) {
+        alert(myName + ' Okay just press CTRL + ALT to talk with me');
+        i = 1;
+    }
+    if(a && b == false) {
+        alert(myName + ' I did not understand you? Could you please respond using yes or no?');
     }
 }
