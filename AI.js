a0e2910d7b761b729018013c05fc0781cf9a21cd
Nick Johnson <nick.orion2003@gmail.com>
2018-11-20 05:09:18 +0000

    Initial commit

--- AI.js
+++ AI.js
@@ -1,17 +1,34 @@
+function startAI(key) {
+    if (key == true) {
+        for(var i = 0; i < 1; i = i) {
+            var response = prompt(myName + ' Do you need to talk to me?');
+            var a = response.includes('Yes');
+            var a2 = response.includes('yes');
+            var b = response.includes('No');
+            var b2 = response.includes('no');
+            if(a == true || a2 == true) {
+                alert(myName + ' I am here for you ' + ownersName + '!');
+                i = 1;
+            }
+            if(b == true || b2 == true) {
+                alert(myName + ' Okay just press CTRL + ALT to talk with me');
+                i = 1;
+            }
+            if(a == false && a2 == false && b == false && b2 == false) {
+                alert(myName + ' I did not understand you? Could you please respond using yes or no?');
+            }
+        }
+    } else {
+        //Do nothing
+    }
+}
+
 alert(myName + ' Hello ' + ownersName + '!');
-for(var i = 0; i < 1; i = i) {
-    var response = prompt(myName + ' Do you need to talk to me?');
-    var a = response.includes('Yes' || 'yes');
-    var b = response.includes('No' || 'no');
-    if(a == true) {
-        alert(myName + ' I am here for you ' + ownersName + '!');
-        i = 1;
-    }
-    if(b == true) {
-        alert(myName + ' Okay just press CTRL + ALT to talk with me');
-        i = 1;
-    }
-    if(a && b == false) {
-        alert(myName + ' I did not understand you? Could you please respond using yes or no?');
+startAI(true);
+
+window.onkeydown = function (e) {
+    if (!e) e = window.event;
+    if (e.ctrlKey && e.altKey) {
+        startAI(true);
     }
 }
